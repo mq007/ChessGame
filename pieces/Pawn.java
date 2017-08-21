18ffec6e9400cd38e5ef397b90cd3eb515d3f626
mq007 <marcin.slowiak.007@gmail.com>
2017-08-21 19:49:50 +0200

    Modification in Pawn getAvailableMoves

--- pieces/Pawn.java
+++ pieces/Pawn.java
@@ -23,30 +23,38 @@ public class Pawn extends Piece {
         List<Position> moveList = new ArrayList<>();
 
         if(getPieceColor().equals(PieceColor.BLACK)){
-            if(y+1 < 8 && board[y+1][x].isFieldFree()){
-                moveList.add(new Position(x, y+1));
+            if(y+1<8){
+                if(board[y+1][x].isFieldFree()){
+                    moveList.add(new Position(x, y+1));
+                }
+                if(x-1>=0 && !board[y+1][x-1].isFieldFree()){
+                    moveList.add(new Position(x-1, y+1));
+                }
+                if(x+1<8 && !board[y+1][x+1].isFieldFree()){
+                    moveList.add(new Position(x+1, y+1));
+                }
             }
-            if(y+2 < 8 && getMoveCounter() == 0 && board[y+2][x].isFieldFree()){
-                moveList.add(new Position(x, y+2));
-            }
-            if(!board[y+1][x-1].isFieldFree()){
-                moveList.add(new Position(x-1, y+1));
-            }
-            if(!board[y+1][x+1].isFieldFree()){
-                moveList.add(new Position(x+1, y+1));
+            if(y+2<8){
+                if(getMoveCounter() == 0 && board[y+2][x].isFieldFree()){
+                    moveList.add(new Position(x, y+2));
+                }
             }
         }else{
-            if(board[y-1][x].isFieldFree()){
-                moveList.add(new Position(x, y-1));
+            if(y-1>=0){
+                if(board[y-1][x].isFieldFree()){
+                    moveList.add(new Position(x, y-1));
+                }
+                if(x-1>=0 && !board[y-1][x-1].isFieldFree()){
+                    moveList.add(new Position(x-1, y-1));
+                }
+                if(x+1<8 && !board[y-1][x+1].isFieldFree()){
+                    moveList.add(new Position(x+1, y-1));
+                }
             }
-            if(getMoveCounter() == 0 && board[y-2][x].isFieldFree()){
-                moveList.add(new Position(x, y-2));
-            }
-            if(!board[y-1][x-1].isFieldFree()){
-                moveList.add(new Position(x-1, y-1));
-            }
-            if(!board[y-1][x+1].isFieldFree()){
-                moveList.add(new Position(x+1, y-1));
+            if(y-2>=0){
+                if(getMoveCounter() == 0 && board[y-2][x].isFieldFree()){
+                    moveList.add(new Position(x, y-2));
+                }
             }
         }
 
